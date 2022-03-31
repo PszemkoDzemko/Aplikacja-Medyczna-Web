444c959cd2f57db65930c06ce9254aa844e1583b
Pszemko-PC <przemek.skrzecz@gmail.com>
2022-03-31 14:43:11 +0200

    website

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
+import { getAuth, 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword,
+  signOut, 
+  setPersistence, 
+  browserLocalPersistence,
+  onAuthStateChanged } from "firebase/auth";
 import { getFirestore, collection, getDocs } from "firebase/firestore"
 
 const firebaseConfig = {
@@ -14,11 +20,68 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth();
 const db = getFirestore()
 
-// const userTable = document.querySelector('#add-users-table');
+//sprawdzanie obecnego użytkownika
+//To trzeba wykonać tylko raz w trakcie załadowania strony
+  onAuthStateChanged(auth,(user)=>{
+    if(user){
+      //użytkownik jest zalogowany
+      //wrzuć go na mainpage
+      
+      //i pobierz jego uid
+      const uid = user.uid
+    }else{
+      //nie jest zalogowany to go wywal na index
+      //window.location.href='index.html';
+    }
+  });
 
+
+
+//Logowanie
+//pobieranie danych
+const loginButton = document.querySelector('#btnLogin');
+const email = document.querySelector('#emailLogin');
+const password = document.querySelector('#passwordLogin');
+if(loginButton!==null){
+  loginButton.addEventListener('click',()=>{
+    //ustaw sesję lokalną
+    setPersistence(auth, browserLocalPersistence)
+    .then(()=>{
+      //zaloguj mailem i hasłem
+      signInWithEmailAndPassword(auth,email.value,password.value)
+      .then((userCredential)=>{
+        window.location.href='mainpage.html';
+        //po zalogowaniu przejdź na mainpage
+      })
+      .catch((error)=>{
+        //tu są błędy jak coś nie działa np. złe hasło czy coś
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      })
+    })
+  })
+}
+
+//wyloguj się
+const btnSignOut = document.querySelector('#btnSignOut');
+if(btnSignOut!==null){
+  btnSignOut.addEventListener('click',()=>{
+    console.log("elo");
+    setPersistence(auth,browserLocalPersistence)
+    .then(()=>{
+      signOut(auth).then(()=>{
+        chcekUserLogin();
+      }).catch((error)=>{
+        //nie udało się wylogować czy coś
+      })
+    })
+  })
+}
+
+// const userTable = document.querySelector('#add-users-table');
 // function renderUsers(doc){
 //   let tr = document.createElement('tr');
 //   let name = document.createElement('td');
@@ -31,39 +94,30 @@ const db = getFirestore()
 //   pesel.textContent = doc.data().pesel;
 //   phone.textContent = doc.data().phone;
 //   address.textContent = doc.data().address;
-  
-
 //   tr.setAttribute('data-id',doc.id);
 //   tr.appendChild(name);
 //   tr.appendChild(surname);
 //   tr.appendChild(pesel);
 //   tr.appendChild(phone);
 //   tr.appendChild(address);
-
 //   userTable.appendChild(tr);
 // } 
-
 // const docTable = document.querySelector('#add-doctor-table');
-
 // function renderDocs(doc){
 //   let tr = document.createElement('tr');
 //   let name = document.createElement('td');
 //   let surname = document.createElement('td');
 //   let specialization = document.createElement('td');
 //   let img = document.createElement('img');
-
-
 //   name.textContent = doc.data().name;
 //   surname.textContent = doc.data().surname;
 //   specialization.textContent = doc.data().specialization
 //   img.src = doc.data().img
-
 //   tr.setAttribute('data-id',doc.id);
 //   tr.appendChild(name);
 //   tr.appendChild(surname);
 //   tr.appendChild(specialization);
 //   tr.appendChild(img);
-
 //   docTable.appendChild(tr);
 // } 
 

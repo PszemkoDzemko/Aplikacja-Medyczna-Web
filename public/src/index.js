5780e859d8a2b779cc880706721404bbed0d54c2
Pszemko-PC <przemek.skrzecz@gmail.com>
2022-05-05 16:50:27 +0200

    potwierdzenie usuwania

--- public/src/index.js
+++ public/src/index.js
@@ -392,8 +392,17 @@ function renderDoneVisits(doc) {
 
 //Usuwanie wizyty--------------------------------------------------
 function deleteVisit(id) {
-  deleteDoc(doc(db, "visits", id));
-  setTimeout(() => { window.location.href = 'mainpage.html' }, 500)
+  const delModal = document.getElementById('delConfirmModal');
+  const conDeleteBtn = document.getElementById('delConfirmButton');
+  const closeDelModal = document.getElementById('closeDelConfirmModal');
+  delModal.showModal();
+  closeDelModal.addEventListener('click', () => {
+    delModal.close()
+  })
+  conDeleteBtn.addEventListener('click',()=>{
+    deleteDoc(doc(db, "visits", id));
+    setTimeout(() => { window.location.href = 'mainpage.html' }, 500)
+  })
 }
 
 //Potwierdzanie wizyty----------------------------------------------
